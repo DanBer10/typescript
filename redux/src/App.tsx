400628fb65cb0c815b06e3b97e27f997a9bda435
DanBer10 <dannebernhardt@outlook.com>
2022-10-03 14:32:55 +0200

    Can see posts by user

--- redux/src/App.tsx
+++ redux/src/App.tsx
@@ -4,8 +4,10 @@ import AddPostForm from "./features/posts/form/AddPostForm";
 import PostsList from "./features/posts/components/PostsList";
 import SinglePostPage from "./features/posts/components/SinglePostPage";
 import EditPostForm from "./features/posts/form/EditPostForm";
+import UsersList from "./features/users/components/UsersList";
+import UserPage from "./features/users/components/UserPage";
 import Layout from "./components/Layout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
@@ -18,6 +20,13 @@ function App() {
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
